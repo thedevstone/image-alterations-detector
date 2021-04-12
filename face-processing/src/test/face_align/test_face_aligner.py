bd343184e1d591f5f0520546fd0688e9ae4eeac9
Luca Giulianini <luca.giulianini2@studio.unibo.it>
2021-04-12 18:20:44 +0200

    Face alignment

--- face-processing/src/test/face_align/test_face_aligner.py
+++ face-processing/src/test/face_align/test_face_aligner.py
@@ -11,13 +11,15 @@ if __name__ == '__main__':
 
     # Images
     img1 = cv2.imread('../../../images/img1.jpg', cv2.IMREAD_COLOR)
-    img2 = cv2.imread('../../../images/img2_rotated.jpg', cv2.IMREAD_COLOR)
+    img1_rotated = cv2.imread('../../../images/img1_rotated.png', cv2.IMREAD_COLOR)
     img1 = cv2.cvtColor(img1, cv2.COLOR_BGR2RGB)
-    img2 = cv2.cvtColor(img2, cv2.COLOR_BGR2RGB)
+    img1_rotated = cv2.cvtColor(img1_rotated, cv2.COLOR_BGR2RGB)
 
-    shape = landmark_extractor.get_2d_landmarks(img1)
-    aligned_image = face_aligner.align(img1, shape)
-    images_to_show = [(img1, "Img"),
-                      (aligned_image, "Aligned")]
-    mosaic = get_images_mosaic("Alignment", images_to_show, rows=1, cols=2)
+    shape1 = landmark_extractor.get_2d_landmarks(img1)
+    shape1_rotated = landmark_extractor.get_2d_landmarks(img1_rotated)
+    aligned_image1 = face_aligner.align(img1, shape1)
+    aligned_image1_rotated = face_aligner.align(img1_rotated, shape1_rotated)
+    images_to_show = [(img1, "Image"), (aligned_image1, "Aligned"),
+                      (img1_rotated, "Rotated"), (aligned_image1_rotated, "Aligned rotated")]
+    mosaic = get_images_mosaic("Alignment", images_to_show, rows=2, cols=2)
     plt.show()
