3ec3787fcb59d713d38ff545dff0c5f561b2f6fa
Luca Giulianini <luca.giulianini2@studio.unibo.it>
2021-04-13 14:19:49 +0200

    Finished triangulation

--- face-processing/src/feature_extraction/triangulation/delaunay.py
+++ face-processing/src/feature_extraction/triangulation/delaunay.py
@@ -1,10 +1,13 @@
-from typing import List
+from typing import List, Tuple
 
 import cv2
 import numpy as np
+from matplotlib import pyplot as plt
+
+import feature_extraction.triangulation.utils as utils
 
 
-def get_triangulations(img: np.ndarray, points: np.ndarray) -> List[np.ndarray]:
+def get_triangulations(img: np.ndarray, points: np.ndarray) -> np.ndarray:
     """ Get triangulation array
     
     :param img: the input image
@@ -21,26 +24,21 @@ def get_triangulations(img: np.ndarray, points: np.ndarray) -> List[np.ndarray]:
         p = tuple(p)
         subdivision.insert(p)
     # Get triangulation
-    triangle_list: List[np.ndarray] = subdivision.getTriangleList()
-    return triangle_list
+    triangles: np.ndarray = subdivision.getTriangleList()
+    return triangles
 
 
-def get_triangulations_indexes(img: np.ndarray, points: np.ndarray):
+def get_triangulations_indexes(img: np.ndarray, points: np.ndarray) -> np.ndarray:
     """ Get triangulation indexes bases on points list
 
     :param img: the input image
     :param points: the landmark points
     :return: a list of tuples ot triangles points
     """
-    triangle_list = get_triangulations(img, points)
-    points_list = []
-    triangle_indexes = []
-    for i, p in enumerate(points):
-        x = points[i][0]
-        y = points[i][1]
-        points_list.append((x, y))
-
-    for t in triangle_list:
+    triangles = get_triangulations(img, points).astype('int')
+    triangles_indexes = np.zeros((len(triangles), 3), dtype='int')
+    points_list: List[Tuple[int, int]] = utils.points_to_list_of_tuple(points)
+    for i, t in enumerate(triangles):
         # Get triangles points
         tri_point1 = (t[0], t[1])
         tri_point2 = (t[2], t[3])
@@ -50,14 +48,18 @@ def get_triangulations_indexes(img: np.ndarray, points: np.ndarray):
         index_tri_pt2 = points_list.index(tri_point2)
         index_tri_pt3 = points_list.index(tri_point3)
         # Append indexes
-        triangle_indexes.append((index_tri_pt1, index_tri_pt2, index_tri_pt3))
-    return triangle_indexes
+        triangles_indexes[i] = [index_tri_pt1, index_tri_pt2, index_tri_pt3]
+    return triangles_indexes
 
 
 if __name__ == '__main__':
     from feature_extraction.landmarks.landmark_extractor import LandmarkExtractor
 
     img = cv2.imread('../../../images/img1.jpg')
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     extractor = LandmarkExtractor("../../../models/shape_predictor_68_face_landmarks.dat")
     points = extractor.get_2d_landmarks(img)
-    get_triangulations_indexes(img, points)
+    triangles_indexes = get_triangulations_indexes(img, points)
+    image_delaunay = utils.draw_delaunay(img, points, triangles_indexes, (150, 0, 0))
+    plt.imshow(image_delaunay)
+    plt.show()
---
+++ face-processing/src/feature_extraction/triangulation/utils.py
@@ -0,0 +1,65 @@
+# Check if a point is inside a rectangle
+from typing import Tuple, List
+
+import cv2
+import numpy as np
+
+
+def points_to_list_of_tuple(points: np.ndarray) -> List[Tuple[int, int]]:
+    points_list = []
+    for i, p in enumerate(points):
+        x = points[i][0]
+        y = points[i][1]
+        points_list.append((x, y))
+    return points_list
+
+
+def triangulation_indexes_to_points(points: np.ndarray, triangles_indexes: np.ndarray) -> np.ndarray:
+    # List of points
+    points_list = points_to_list_of_tuple(points)
+    triangles = np.zeros((len(triangles_indexes), 6), dtype='int')
+    for i, t in enumerate(triangles_indexes):
+        tri_point1: Tuple[int, int] = points_list[t[0]]
+        tri_point2: Tuple[int, int] = points_list[t[1]]
+        tri_point3: Tuple[int, int] = points_list[t[2]]
+        triangles[i] = [tri_point1[0],
+                        tri_point1[1],
+                        tri_point2[0],
+                        tri_point2[1],
+                        tri_point3[0],
+                        tri_point3[1]]
+    return triangles
+
+
+def rect_contains(rect, point):
+    if point[0] < rect[0]:
+        return False
+    elif point[1] < rect[1]:
+        return False
+    elif point[0] > rect[2]:
+        return False
+    elif point[1] > rect[3]:
+        return False
+    return True
+
+
+def draw_delaunay(img: np.ndarray, points: np.ndarray, triangles_indexes: np.ndarray,
+                  delaunay_color: Tuple[int, int, int]) -> np.ndarray:
+    # Get shape of image
+    size = img.shape
+    r = (0, 0, size[1], size[0])
+    # List of points
+    triangles = triangulation_indexes_to_points(points, triangles_indexes)
+    # Output image
+    image_out = img.copy()
+    # Find points coordinates
+    for t in triangles:
+        tri_point1 = (t[0], t[1])
+        tri_point2 = (t[2], t[3])
+        tri_point3 = (t[4], t[5])
+
+        if rect_contains(r, tri_point1) and rect_contains(r, tri_point2) and rect_contains(r, tri_point3):
+            cv2.line(image_out, tri_point1, tri_point2, delaunay_color, 1, cv2.LINE_AA, 0)
+            cv2.line(image_out, tri_point2, tri_point3, delaunay_color, 1, cv2.LINE_AA, 0)
+            cv2.line(image_out, tri_point3, tri_point1, delaunay_color, 1, cv2.LINE_AA, 0)
+    return image_out
