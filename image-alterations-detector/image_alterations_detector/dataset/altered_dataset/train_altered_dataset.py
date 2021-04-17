d519e12447aea89ce45b47e581bb6cd6828f3e6a
Luca Giulianini <luca.giulianini2@studio.unibo.it>
2021-04-17 18:46:10 +0200

    Finished training

--- image-alterations-detector/image_alterations_detector/dataset/altered_dataset/train_altered_dataset.py
+++ image-alterations-detector/image_alterations_detector/dataset/altered_dataset/train_altered_dataset.py
@@ -9,23 +9,45 @@ def train_altered_descriptors():
     dataset_path = '/Users/luca/Desktop/altered'
     mean_area_descriptors, matrices_descriptors, lbp_descriptors, labels = compute_altered_descriptors(dataset_path)
 
-    descriptors: np.ndarray = np.column_stack([matrices_descriptors, lbp_descriptors])
-    print("Big descriptor shape:", descriptors.shape)
-    x_train_descriptors, x_test_descriptors, y_train_descriptors, y_test_descriptors = train_test_split(descriptors,
-                                                                                                        labels,
-                                                                                                        test_size=0.2,
-                                                                                                        random_state=23)
-    # Train
-    multi_clf_matrices = MlpSvmRf()
-    multi_clf_matrices.create_model(x_test_descriptors.shape[1])
-    multi_clf_matrices.fit(x_train_descriptors, y_train_descriptors)
-    multi_clf_matrices.evaluate(x_test_descriptors, y_test_descriptors)
-    multi_clf_matrices.save_models('total_descriptors')
-    to_predict = np.expand_dims(x_test_descriptors[0], 0)
+    # descriptors: np.ndarray = np.column_stack([matrices_descriptors, lbp_descriptors])
+    # print("Big descriptor shape:", descriptors.shape)
+    print("Matrices shape:", matrices_descriptors.shape)
+    x_train_matrices_descriptors, x_test_matrices_descriptors, y_train_matrices_descriptors, y_test_matrices_descriptors = train_test_split(
+        matrices_descriptors,
+        labels,
+        test_size=0.2,
+        random_state=23)
 
-    # Test on two images
-    result = multi_clf_matrices.predict_one(to_predict)
-    print(result)
+    # Train on matrices
+    multi_clf_matrices = MlpSvmRf()
+    multi_clf_matrices.create_model(x_train_matrices_descriptors.shape[1])
+    multi_clf_matrices.fit(x_train_matrices_descriptors, y_train_matrices_descriptors)
+    multi_clf_matrices.evaluate(x_test_matrices_descriptors, y_test_matrices_descriptors)
+    multi_clf_matrices.save_models('matrices_descriptors')
+
+    print("LBP shape:", lbp_descriptors.shape)
+    x_train_lbp_descriptors, x_test_lbp_descriptors, y_train_lbp_descriptors, y_test_lbp_descriptors = train_test_split(
+        lbp_descriptors,
+        labels,
+        test_size=0.2,
+        random_state=23)
+
+    # Train on lbp
+    multi_clf_lbp = MlpSvmRf()
+    multi_clf_lbp.create_model(x_train_lbp_descriptors.shape[1])
+    multi_clf_lbp.fit(x_train_lbp_descriptors, y_train_lbp_descriptors)
+    multi_clf_lbp.evaluate(x_test_lbp_descriptors, y_test_lbp_descriptors)
+    multi_clf_lbp.save_models('lbp_descriptors')
+
+    # Test Matrices
+    to_predict_matrices = np.expand_dims(x_test_matrices_descriptors[0], 0)
+    result_matrices = multi_clf_matrices.predict_one(to_predict_matrices)
+    print(result_matrices)
+
+    # Test Lbp
+    to_predict_lbp = np.expand_dims(x_test_lbp_descriptors[0], 0)
+    result_lbp = multi_clf_lbp.predict_one(to_predict_lbp)
+    print(result_lbp)
 
 
 if __name__ == '__main__':
