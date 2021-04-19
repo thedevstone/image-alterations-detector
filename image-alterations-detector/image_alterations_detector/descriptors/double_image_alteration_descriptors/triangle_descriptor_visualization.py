3aa71c066a9a9ba47804b476efb502f9b6cdfc17
Luca Giulianini <luca.giulianini2@studio.unibo.it>
2021-04-20 00:30:05 +0200

    Finished delaunay distance

---
+++ image-alterations-detector/image_alterations_detector/descriptors/double_image_alteration_descriptors/triangle_descriptor_visualization.py
@@ -0,0 +1,82 @@
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.preprocessing import minmax_scale
+
+from image_alterations_detector.descriptors.triangle_descriptors.triangle_descriptors import \
+    compute_triangle_affine_matrix
+from image_alterations_detector.face_morphology.landmarks_triangulation.conversions import \
+    triangulation_indexes_to_points
+from image_alterations_detector.face_morphology.landmarks_triangulation.manage_triangulation import load_triangulation
+from image_alterations_detector.face_transform.face_alignment.face_aligner import FaceAligner
+
+
+def draw_delaunay_alterations(source_image, dest_image, animate=True) -> np.ndarray:
+    aligner = FaceAligner(desired_face_width=512)
+    # Align
+    source_image, source_image_landmarks = aligner.align(source_image)
+    dest_image, dest_image_landmarks = aligner.align(dest_image)
+    # Extract indexes from one image
+    triangles_indexes = load_triangulation('triangulation.txt')
+    source_image_landmarks = triangulation_indexes_to_points(source_image_landmarks, triangles_indexes)
+    dest_image_landmarks = triangulation_indexes_to_points(dest_image_landmarks, triangles_indexes)
+    # Sort triangles
+    t1s, t2s = sort_altered_triangles(source_image_landmarks, dest_image_landmarks)
+    # Find points coordinates
+    # Output image
+    source_out = source_image.copy()
+    import colorsys
+
+    for idx, t in enumerate(t1s):
+        color_index = len(t1s) - idx
+        rgb = colorsys.hsv_to_rgb(color_index / 300., 1.0, 1.0)
+        delaunay_color = tuple([round(255 * x) for x in rgb])
+
+        tri_point1 = (t[0], t[1])
+        tri_point2 = (t[2], t[3])
+        tri_point3 = (t[4], t[5])
+
+        cv2.line(source_out, tri_point1, tri_point2, delaunay_color, 1, cv2.LINE_AA, 0)
+        cv2.line(source_out, tri_point2, tri_point3, delaunay_color, 1, cv2.LINE_AA, 0)
+        cv2.line(source_out, tri_point3, tri_point1, delaunay_color, 1, cv2.LINE_AA, 0)
+
+        if animate:
+            cv2.imshow('Delaunay', cv2.cvtColor(source_out, cv2.COLOR_BGR2RGB))
+            cv2.waitKey(300)
+    plt.imshow(source_out)
+    plt.show()
+    return source_out
+
+
+def sort_altered_triangles(source_image_landmarks, dest_image_landmarks):
+    # Affine matrices
+    matrices_distances = []
+    source1_triangle_number = len(source_image_landmarks)
+    source2_triangle_number = len(dest_image_landmarks)
+    if source1_triangle_number != source2_triangle_number:
+        raise AssertionError('Images have different number of triangles')
+    for t1, t2 in zip(source_image_landmarks, dest_image_landmarks):
+        affine_matrix = compute_triangle_affine_matrix(t1, t2)
+        matrices_distances.append(affine_matrix.flatten())
+    # Normalize affine matrices
+    matrices_distances = np.array(matrices_distances)
+    matrices_distances = minmax_scale(matrices_distances)
+    # Computing mean matrix
+    mean_matrix = matrices_distances.mean(axis=0)
+    # Preparing for sort according to distance
+    distances = []
+    t1s = []
+    t2s = []
+    # Computing distance between matrices and mean matrix
+    for matrix, t1, t2 in zip(matrices_distances, source_image_landmarks, dest_image_landmarks):
+        dist = np.linalg.norm(matrix - mean_matrix)
+        distances.append(dist)
+        t1s.append(tuple(t1))
+        t2s.append(tuple(t2))
+    # Sorting lists according to distances
+    t1s = [x for y, x in sorted(zip(distances, t1s))]
+    t2s = [x for y, x in sorted(zip(distances, t2s))]
+    distances.sort()
+    t1s = np.array(t1s)
+    t2s = np.array(t2s)
+    return t1s, t2s
---
+++ image-alterations-detector/image_alterations_detector/test/descriptors/test_visualize_altered_triangles.py
@@ -0,0 +1,19 @@
+from image_alterations_detector.descriptors.double_image_alteration_descriptors.triangle_descriptor_visualization import \
+    draw_delaunay_alterations
+from image_alterations_detector.utils.image_utils import load_img
+
+
+def main():
+    # dataset_path = '/Users/luca/Desktop/altered'
+    # genuine, beauty = load_altered_dataset(dataset_path)
+    # image_index = 3
+    # print('Test intra dataset')
+    # genuine_img = genuine[0][image_index]
+    # controlled_Img = genuine[2][image_index]
+    # beauty_img = beauty[2][image_index]
+
+    draw_delaunay_alterations(load_img('m-001-1.png'), load_img('m-001-a.jpg'))
+    
+
+if __name__ == '__main__':
+    main()
