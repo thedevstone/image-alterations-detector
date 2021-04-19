a82174128a25ed9ee66013b5ea84fd88729ede48
Luca Giulianini <luca.giulianini2@studio.unibo.it>
2021-04-19 15:57:06 +0200

    Refactored segmentation

--- image-alterations-detector/image_alterations_detector/test/segmentation/test_segmentation.py
+++ image-alterations-detector/image_alterations_detector/test/segmentation/test_segmentation.py
@@ -1,40 +1,9 @@
-from image_alterations_detector.face_transform.face_alignment.face_aligner import FaceAligner
-from image_alterations_detector.file_system.path_utilities import get_image_path, get_model_path
-from image_alterations_detector.segmentation.configuration.keras_backend import set_keras_backend
+from matplotlib import pyplot as plt
 
-
-def main():
-    set_keras_backend()
-    import cv2
-    import matplotlib.pyplot as plt
-    import image_alterations_detector.segmentation.configuration.color_configuration as color_conf
-    import image_alterations_detector.segmentation.conversions as conversions
-    import image_alterations_detector.segmentation.model as model
-
-    # Configuration
-    image_size = 256
-    aligner = FaceAligner(desired_face_width=image_size)
-    classes_to_segment = {'skin': True, 'nose': True, 'eye': True, 'brow': True, 'ear': True, 'mouth': True,
-                          'hair': True, 'neck': True, 'cloth': False}
-    classes_list, colors_values_list = color_conf.get_classes_colors(classes_to_segment)
-
-    # Images
-    img1 = cv2.imread(get_image_path('img1.jpg'), cv2.IMREAD_COLOR)
-    img1 = cv2.cvtColor(img1, cv2.COLOR_BGR2RGB)
-    img1, landmarks = aligner.align(img1)
-    img1 = img1.reshape((1, img1.shape[0], img1.shape[1], img1.shape[2])).astype('float')
-    img1_normalized = img1 / 255.0
-
-    # Predict
-    inference_model = model.load_model(get_model_path('unet.h5'))
-
-    images_predicted = inference_model.predict(img1_normalized)
-    predicted = conversions.denormalize(images_predicted[0])
-    predicted_rgb = conversions.mask_channels_to_rgb(predicted, 8, image_size, colors_values_list)
-    plt.imshow(predicted_rgb)
-    plt.axis('off')
-    plt.show()
-
+from image_alterations_detector.segmentation.test_segmentation import segment_image
 
 if __name__ == '__main__':
-    main()
+    segmented = segment_image('img1.jpg')
+    plt.imshow(segmented)
+    plt.axis('off')
+    plt.show()
